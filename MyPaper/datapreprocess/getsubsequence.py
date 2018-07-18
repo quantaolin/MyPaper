52aa62962be99c317ad7a663768b515df137937f
quantaolin <quantaolin@bogon>
2018-07-18 19:24:44 +0800

    1

--- MyPaper/datapreprocess/getsubsequence.py
+++ MyPaper/datapreprocess/getsubsequence.py
@@ -5,7 +5,8 @@ Created on 2018-07-17 22:07
 '''
 from pymongo import MongoClient
 
-
+retracement_tolerance = 0.2
+rebound_trend_confirm = 0.3
 
 conn = MongoClient('127.0.0.1', 27017)
 db = conn.mydb

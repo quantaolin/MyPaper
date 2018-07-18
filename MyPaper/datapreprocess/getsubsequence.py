1118844754d79c483f1864da58b534a69e039e53
linqt <linqt@windows10.microdone.cn>
2018-07-19 00:56:51 +0800

    1

--- MyPaper/datapreprocess/getsubsequence.py
+++ MyPaper/datapreprocess/getsubsequence.py
@@ -8,6 +8,9 @@ from pymongo import MongoClient
 retracement_tolerance = 0.2
 rebound_trend_confirm = 0.3
 
+def getSubSeque(pricelist):
+    return
+
 conn = MongoClient('127.0.0.1', 27017)
 db = conn.mydb
 sb_set = db.sb_set

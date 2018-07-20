3f1a15401e6fb171241e4aa44224cc8c351e6c7a
quantaolin <quantaolin@bogon>
2018-07-20 17:19:58 +0800

    1

--- MyPaper/datapreprocess/getsubsequence.py
+++ MyPaper/datapreprocess/getsubsequence.py
@@ -14,33 +14,65 @@ def getSubSeque(pricelist):
     last_max_index=0
     last_max_value=-1
     riseandfall_flag=0
-    key_point_indexs=[0]
+    key_point_indexs=[]
     key_point_riseandfall_flags=[]
     for index in range(len(pricelist)):
         value = pricelist[index]
         if index == 0: 
             last_min_value = last_max_value = value
             continue
+        if value < last_min_value:
+            last_min_index = index
+            last_min_value = value
+        elif value > last_max_value:
+            last_max_index = index
+            last_max_value = value
         if riseandfall_flag == 0:
-            if value < last_min_value:
-                last_min_index = index
-                last_min_value = value
-                riseandfall_flag = -1
+            if (value - last_min_value)/last_min_value > rebound_trend_confirm:
+                riseandfall_flag = 1
+                key_point_indexs.append(last_min_index)
                 key_point_riseandfall_flags.append(riseandfall_flag)
-            elif value > last_max_value:
                 last_max_index = index
                 last_max_value = value
-                riseandfall_flag = 1
-                key_point_riseandfall_flags.append(riseandfall_flag)          
+            if (last_max_value - value)/last_max_value > retracement_tolerance:
+                riseandfall_flag = -1
+                key_point_indexs.append(last_max_index)
+                key_point_riseandfall_flags.append(riseandfall_flag)
+                last_min_index = index
+                last_min_value = value   
         elif riseandfall_flag > 0:
-            
+            if (last_max_value - value)/last_max_value > retracement_tolerance:
+                riseandfall_flag = -1
+                key_point_indexs.append(last_max_index)
+                key_point_riseandfall_flags.append(riseandfall_flag)
+                last_min_index = index
+                last_min_value = value
         elif riseandfall_flag < 0:
-            
+            if (value - last_min_value)/last_min_value > rebound_trend_confirm:
+                riseandfall_flag = 1
+                key_point_indexs.append(last_min_index)
+                key_point_riseandfall_flags.append(riseandfall_flag)
+                last_max_index = index
+                last_max_value = value
     return key_point_indexs,key_point_riseandfall_flags
 
 conn = MongoClient('127.0.0.1', 27017)
 db = conn.mydb
 sb_set = db.sb_set
+seq_set = db.seq_set
 
 for i in sb_set.find():
     code = i['code']
+    
+    collist = db.collection_names()
+    if code not in collist:
+        print("code:",code," is not have continue")
+        continue
+    
+    tmp_set = db[code]
+    pricelist = [] 
+    for j in tmp_set.find().sort("data"):
+        pricelist.append(j['close'])
+        
+    key_point_indexs,key_point_riseandfall_flags = getSubSeque(pricelist)
+    seq_set.insert({"code":code,"key_point_indexs":key_point_indexs,"key_point_riseandfall_flags":key_point_riseandfall_flags})

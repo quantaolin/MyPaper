bf838eedc54c891890b1ed339b0b30d1927d4379
linqt <linqt@windows10.microdone.cn>
2018-10-11 00:38:51 +0800

    1

--- MyPaper/dataprocess/subsequencedtw.py
+++ MyPaper/dataprocess/subsequencedtw.py
@@ -6,3 +6,18 @@ Created on 2018-08-19 01:54
 
 def deal(querySeq,majorSeq):
     return "dist","path"
+
+def getCostMatrix(querySeq,majorSeq):
+    costMatrix = [[0]*len(majorSeq) for i in range(len(querySeq))]
+    for p in range(len(querySeq)):
+        row=[]
+        queryIndex = querySeq[p]
+        for q in range(len(majorSeq)):
+            majorIndex = majorSeq[q]
+            distance = abs(queryIndex - majorIndex)
+            row.append(distance)
+        costMatrix.append(row)
+    return costMatrix
+
+def getDtw(costMatrix,b):
+    pass

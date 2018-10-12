e92bcba7fff4b7df671764e1a7066843354eeb80
quantaolin <quantaolin@bogon>
2018-10-12 16:42:59 +0800

    1

--- MyPaper/dataprocess/subsequencedtw.py
+++ MyPaper/dataprocess/subsequencedtw.py
@@ -5,7 +5,17 @@ Created on 2018-08-19 01:54
 '''
 
 def deal(querySeq,majorSeq):
-    return "dist","path"
+    xIndex = len(querySeq) - 1
+    maxY = len(majorSeq) - 1
+    dist = float('inf')
+    path = []
+    costMatrix = getCostMatrix(querySeq,majorSeq)
+    for i in range(maxY + 1):
+        tmpDist,tmpPath = getDtw(costMatrix,xIndex,maxY-i)
+        if tmpDist < dist:
+            dist = tmpDist
+            path = tmpPath
+    return dist,path
 
 def getCostMatrix(querySeq,majorSeq):
     costMatrix = [[0]*len(majorSeq) for i in range(len(querySeq))]
@@ -20,4 +30,19 @@ def getCostMatrix(querySeq,majorSeq):
     return costMatrix
 
 def getDtw(costMatrix,xIndex,yIndex):
-    pass
+    if xIndex == 0:
+        return costMatrix[xIndex][yIndex],[[xIndex,yIndex]]
+    elif yIndex == 0:
+        nextDistance,nextPath = getDtw(costMatrix,xIndex-1,yIndex)
+        return costMatrix[xIndex][yIndex]+nextDistance,[[xIndex,yIndex]].extend(nextPath)
+    else :
+        nextDistance,nextPath = getDtw(costMatrix,xIndex-1,yIndex)
+        nextDistance2,nextPath2 = getDtw(costMatrix,xIndex-1,yIndex-1)
+        if nextDistance2 < nextDistance:
+            nextDistance = nextDistance2
+            nextPath = nextPath2
+        nextDistance3,nextPath3 = getDtw(costMatrix,xIndex,yIndex-1)
+        if nextDistance3 < nextDistance:
+            nextDistance = nextDistance3
+            nextPath = nextPath3
+        return costMatrix[xIndex][yIndex]+nextDistance,[[xIndex,yIndex]].extend(nextPath)

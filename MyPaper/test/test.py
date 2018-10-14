a79c68f6571a6dc41923807e2b1224cda7937761
quantaolin <linqt2006@163.com>
2018-10-14 09:50:39 +0800

    1

--- MyPaper/test/test.py
+++ MyPaper/test/test.py
@@ -3,3 +3,14 @@ Created on 2018-08-11 23:41
 
 @author: quantaolin
 '''
+from pymongo import MongoClient
+import math
+import dataprocess.subsequencedtw
+
+DTW_DISTANCE_THRESHOLD=10
+
+conn = MongoClient('127.0.0.1', 27017)
+db = conn.mydb
+sb_set = db.test_sb_set
+rise_feature_set = db.rise_feature_set
+fall_feature_set = db.fall_feature_set

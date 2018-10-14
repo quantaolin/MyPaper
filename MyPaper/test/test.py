af52b249d151434e6d24132215b103552804cd69
quantaolin <linqt2006@163.com>
2018-10-14 16:30:53 +0800

    1

--- MyPaper/test/test.py
+++ MyPaper/test/test.py
@@ -5,12 +5,66 @@ Created on 2018-08-11 23:41
 '''
 from pymongo import MongoClient
 import math
-import dataprocess.subsequencedtw
+from dataprocess import subsequencedtw
 
 DTW_DISTANCE_THRESHOLD=10
+retracement_tolerance = 0.2
+rebound_trend_confirm = 0.3
+
+def getResult(featurePriceSeq,testPriceSeq,riseAndFallFlag,code):
+    costMatrix = subsequencedtw.getCostMatrix(featurePriceSeq, testPriceSeq)
+    xIndex = len(featurePriceSeq) - 1
+    maxY = len(testPriceSeq) - 1
+    for i in range(maxY + 1):
+        tmpDist,tmpPath = subsequencedtw.getDtw(costMatrix,xIndex,maxY-i)
+        if tmpDist < DTW_DISTANCE_THRESHOLD:
+            test_result_set.insert({"code":code,"riseAndFallFlag":riseAndFallFlag,"index":maxY-i})
 
 conn = MongoClient('127.0.0.1', 27017)
 db = conn.mydb
 sb_set = db.test_sb_set
+test_result_set = db.test_result_set
+all_sb_set = db.sb_set
 rise_feature_set = db.rise_feature_set
 fall_feature_set = db.fall_feature_set
+
+print("get price list")
+pricederivatDict={}
+priceDict={}
+for i in all_sb_set.find():
+    code = i['code']
+    collist = db.collection_names()
+    if code not in collist:
+        print("code:",code," is not have continue")
+        continue   
+    tmp_set = db[code]
+    pricederivatlist = [] 
+    pricelist = [] 
+    for j in tmp_set.find().sort("data"):
+        pricederivatlist.append(j['derivative'])
+        pricelist.append(j['close'])      
+    pricederivatDict[code]=pricederivatlist
+    priceDict[code]=pricelist
+    
+print("get feature set")
+riseFeatureDict={}
+fallFeatureDict={}
+for i in rise_feature_set.find():
+    riseFeatureDict[i['code']]=[i['startindex'],i['endindex']]
+for i in fall_feature_set.find():
+    fallFeatureDict[i['code']]=[i['startindex'],i['endindex']]
+
+print("deal test data")
+for i in sb_set.find():
+    code = i['code']
+    collist = db.collection_names()
+    if code not in collist:
+        print("code:",code," is not have continue")
+        continue
+    testPriceSeq = pricederivatDict[code]
+    for key,value in riseFeatureDict.items():
+        featurePriceSeq = pricederivatDict[key][value[0],value[1]]
+        getResult(featurePriceSeq,testPriceSeq,1,code)
+    for key,value in fallFeatureDict.items():
+        featurePriceSeq = pricederivatDict[key][value[0],value[1]]
+        getResult(featurePriceSeq,testPriceSeq,-1,code)

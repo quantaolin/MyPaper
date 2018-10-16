724530792d8bf2caa242421cf1b6ab782908b8f0
linqt <linqt@windows10.microdone.cn>
2018-10-17 00:45:56 +0800

    1

--- MyPaper/dataprocess/deal.py
+++ MyPaper/dataprocess/deal.py
@@ -44,7 +44,7 @@ def getseqentropy(queryseq,trueDict,falseDict,pricederivatDict):
         pricederivatList = pricederivatDict[key]
         for indexgroup in value:
             mainpriceseq = pricederivatList[indexgroup[0]:indexgroup[1]+1]
-            dist, cost, path = subsequencedtw.deal(queryseq, mainpriceseq)
+            dist, path = subsequencedtw.deal(queryseq, mainpriceseq)
             print("false dist:",dist,"code:",key,",startindex:",indexgroup[0],",endindex:",indexgroup[1])
             if dist <= DTW_DISTANCE_THRESHOLD:
                 false_to_true_count += 1

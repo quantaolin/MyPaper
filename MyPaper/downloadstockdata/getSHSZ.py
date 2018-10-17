9a01377862504fe51990fd4a43d4b747d6975cfb
linqt <linqt@windows10.microdone.cn>
2018-10-17 21:47:18 +0800

    1

--- MyPaper/downloadstockdata/getSHSZ.py
+++ MyPaper/downloadstockdata/getSHSZ.py
@@ -20,17 +20,17 @@ testset.insert({"code":"399001test"})
 sbshtrain = ts.get_k_data("000001",start= "2003-01-01",end= '2012-12-31')
 shtrain_set = db['000001train']
 for indx,item in sbshtrain.iterrows():
-        shtrain_set.insert({"data":item['date'],"open":item['open'],"close":item['close'],"volume":item['volume'],"high":item['high'],"low":item['low']})  
+        shtrain_set.insert_one({"data":item['date'],"open":item['open'],"close":item['close'],"volume":item['volume'],"high":item['high'],"low":item['low']})  
 sbshtest = ts.get_k_data("000001",start= "2013-01-01",end= '2018-10-01')
 shtest_set = db['000001test']
 for indx,item in sbshtest.iterrows():
-        shtest_set.insert({"data":item['date'],"open":item['open'],"close":item['close'],"volume":item['volume'],"high":item['high'],"low":item['low']}) 
+        shtest_set.insert_one({"data":item['date'],"open":item['open'],"close":item['close'],"volume":item['volume'],"high":item['high'],"low":item['low']}) 
 sbsztrain = ts.get_k_data("399001",start= "2003-01-01",end= '2012-12-31')
 sztrain_set = db['399001train']
 for indx,item in sbsztrain.iterrows():
-        sztrain_set.insert({"data":item['date'],"open":item['open'],"close":item['close'],"volume":item['volume'],"high":item['high'],"low":item['low']}) 
+        sztrain_set.insert_one({"data":item['date'],"open":item['open'],"close":item['close'],"volume":item['volume'],"high":item['high'],"low":item['low']}) 
 sbsztest = ts.get_k_data("399001",start= "2013-01-01",end= '2018-10-01')
 sztest_set = db['399001test']
 for indx,item in sbsztest.iterrows():
-        sztest_set.insert({"data":item['date'],"open":item['open'],"close":item['close'],"volume":item['volume'],"high":item['high'],"low":item['low']}) 
+        sztest_set.insert_one({"data":item['date'],"open":item['open'],"close":item['close'],"volume":item['volume'],"high":item['high'],"low":item['low']}) 
 

4a147d8675f42113223e88dfef8b8740d26b6d45
quantaolin <quantaolin@bogon>
2018-10-17 12:03:51 +0800

    1

--- MyPaper/downloadstockdata/getSHSZ.py
+++ MyPaper/downloadstockdata/getSHSZ.py
@@ -11,6 +11,9 @@ conn = MongoClient('127.0.0.1', 27017)
 db = conn.mydb
  
 sb_SHSZ_set = db.sb_SHSZ_set
-sb = ts.get_hist_data("000001",start= "2018-04-01",end= '2018-07-06')
+sbshtrain = ts.get_k_data("000001",start= "2003-01-01",end= '2012-12-31')
+sbshtest = ts.get_k_data("000001",start= "2013-01-01",end= '2018-10-01')
+sbsztrain = ts.get_k_data("399001",start= "2003-01-01",end= '2012-12-31')
+sbsztest = ts.get_k_data("399001",start= "2013-01-01",end= '2018-10-01')
 
-print(sb)
+print(sbshtrain)

ced9d6c8ba7507a41192aa50d654094bdcf595fb
quantaolin <quantaolin@bogon>
2018-11-05 16:40:38 +0800

    1

--- MyPaper/test/plot.py
+++ MyPaper/test/plot.py
@@ -3,3 +3,13 @@ Created on  2018-11-01 17:35:37
 
 @author: quantaolin
 '''
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+a = [1, 2, 5, 3, 4]
+b = np.array(a)
+plt.plot(b)
+plt.grid(True) ##增加格点
+plt.axis('tight')
+plt.show()
